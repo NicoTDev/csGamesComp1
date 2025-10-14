f43474105873801a68ac777410ac579525b7395b
Nicolas Tremblay <111556359+NicoTDev@users.noreply.github.com>
2025-10-14 19:47:15 -0400

    Add files via upload

--- file.sh
+++ file.sh
@@ -7,8 +7,20 @@ IFS=$'\n'
 for LINE in $(cat "$INFILE")
 do
     IFS=', ' read -r -a array <<< $LINE
+    if [ -z "${array[0]}" ]; then
+        echo nombreIncorrect
+    fi
+    if [ -z "${array[1]}" ]; then
+        echo Nom incorrect
+    fi
+    if [ -z "${array[2]}" ]; then
+        echo email incorrect
+    fi
     if [ -z "${array[3]}" ]; then
-        echo emailEmpty!
+        echo ville incorrecte
+    fi
+    if [ -z "${array[4]}" ]; then
+        echo Status incorrect
     fi
 
 done
